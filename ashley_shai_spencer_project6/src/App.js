e5c42b7ee51ccef4d107148b5ec424d9a7de326a
Spencer <spencerrca@gmail.com>
2018-09-05 15:17:46 -0400

    added basic autosuggest feature

--- ashley_shai_spencer_project6/src/App.js
+++ ashley_shai_spencer_project6/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// COMPONENTS
+import AutoSuggest from './components/AutoSuggest'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Sug Fun!</h1>
+        <AutoSuggest />
       </div>
     );
   }
