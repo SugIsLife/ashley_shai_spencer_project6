e631e6745369713d9ae776fc091e80efcc947ffc
Ashley <ashleyhellyer@gmail.com>
2018-09-07 17:38:30 -0400

    worked on Poem component path and pushing/pulling from firebase

--- ashley_shai_spencer_project6/src/components/Poem.js
+++ ashley_shai_spencer_project6/src/components/Poem.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import firebase from './firebase';
 
-const Poem = (props) => {
-  console.log(props.selectedWords);
-
+class Poem extends Component {
+  constructor(){
+    super()
+    this.state = {
+      poemArray = [],
+    }
+  }
+  // console.log(props.selectedWords);
+  componentDidMount = () => {
+    const fullPath = this.props.location.pathname;
+    const poemKey = fullPath.split('/')[2];
+    console.log(poemKey);
+    const dbRef = firebase.database().ref(`/${poemKey}`);
+    console.log(dbRef);
+    dbRef.once('value').then((snapshot) => {
+      console.log(snapshot.val())
+      poemArray = snapshot.val();
+    })
+    // this needs to be fixed
+    this.setState({
+      poemArray: poemArray;
+    })
+  }
+ 
+  render() {
   return (
     <div>
       <h1>POEM SECTION</h1>
       <Link to="/" >Back</Link>
       <ul>
-        {
-          props.selectedWords.map((word) => {
+        { poemArray.length > 0 ?
+          poemArray.map((word) => {
+            console.log(word);
             return(
               <li className="show">{word}</li>
             )
           })
+          : null
         }
       </ul>
     </div>
   )
+}
 }
 
 export default Poem;
