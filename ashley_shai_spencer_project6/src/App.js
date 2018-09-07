7f9c01ea10369b42a6a1fdafd96344e2c0170de0
Shai <shaistakirmani@gmail.com>
2018-09-07 17:42:17 -0400

    this needs to be fixed

--- ashley_shai_spencer_project6/src/App.js
+++ ashley_shai_spencer_project6/src/App.js
@@ -37,46 +37,6 @@ class App extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <div className="wrapper">
-          <main>
-            <section className="fridge-container">
-              <h1>Magnetic Poetry</h1>
-              <Form passChildState={this.passChildState} queryInput={this.state.queryInput} />
-              <ul>
-                {
-                  this.state.selectedWords
-                  ?
-                  this.state.selectedWords.map((word)=>{
-                    return(
-                      <li onClick={this.removeFromFridge}>{word}</li>
-                    )
-                  }):
-                  null
-                }
-              </ul>
-            </section>
-            <aside className="poem-dashboard">
-              <button className="reset" onClick={this.resetPage}>Reset</button>
-              <button className="share-poem">Share Poem</button>
-              <section className="word-container">
-                <ul className="word-list" id="word-list">
-                  {/* map through the word choices array and create an li for each word  */}
-                  {
-                    this.state.wordList
-                      ?
-                      this.state.wordList.map((word) => {
-                        return (
-                          <li className={this.state.show} onClick={this.addToFridge}>{word}</li>
-                        )
-                      })
-                      :
-                      null
-                  }
-                </ul>
-              </section>
-=======
       <Router>
         <div className="App">
           <div className="wrapper">
@@ -85,7 +45,6 @@ class App extends Component {
               <Route exact path="/" render={(props) =>
                 <Form {...props} passChildState={this.passChildState} queryInput={this.state.queryInput} />
               }/>
->>>>>>> b683944a02ab1c166fc9ecaf25125877df87fe9e
 
               <Route exact path="/fridge" render={(props) =>
                 <Fridge {...props} wordList={this.state.wordList} passChildState={this.passChildState}/>
