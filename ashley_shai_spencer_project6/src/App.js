398936ab6209607379265a6aec8c54e99dbf85da
Spencer <spencerrca@gmail.com>
2018-09-12 09:36:41 -0400

    refactored scss

--- ashley_shai_spencer_project6/src/App.js
+++ ashley_shai_spencer_project6/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 // COMPONENTS
 import Form from './components/Form';
